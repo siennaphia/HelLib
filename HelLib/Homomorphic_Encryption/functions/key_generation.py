6472fbf14aa5cded7314e952475bed411a50f578
Carlo Leiva <87499194+Vidacelinda@users.noreply.github.com>
2024-01-20 17:28:29 -0500

    refactor
    typo fix

--- HelLib/Homomorphic_Encryption/functions/key_generation.py
+++ HelLib/Homomorphic_Encryption/functions/key_generation.py
@@ -1,18 +1,12 @@
 from seal import (
-    EncryptionParameters,
-    scheme_type,
-    Plaintext,
-    Ciphertext,
     KeyGenerator,
-    Encryptor,
 )
 
 def KeyGenerator(context):
-    # public key and secret generated from KeyGeneratoer 
+    # public key and secret key generated from KeyGeneratoer 
     keygen = KeyGenerator(context)
     public_key = keygen.public_key()
     secret_key = keygen.secret_key()
 
     return public_key,secret_key
-
 
