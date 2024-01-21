0d33983461a58c1673a9ed7696fac7c003c149da
Carlo Leiva <87499194+Vidacelinda@users.noreply.github.com>
2024-01-21 14:09:26 -0500

    Revert "refactor"
    This reverts commit e2d7e397347cd7cb485570d14706d4c2862f8602.

--- HelLib/Homomorphic_Encryption/functions/key_generation.py
+++ HelLib/Homomorphic_Encryption/functions/key_generation.py
@@ -2,7 +2,7 @@ from seal import (
     KeyGenerator,
 )
 
-def Key_generator(context):
+def KeyGenerator(context):
     # public key and secret key generated from KeyGeneratoer 
     keygen = KeyGenerator(context)
     public_key = keygen.public_key()
